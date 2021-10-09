3f4e7f366d9be0633d8d81412830d785ec8206b8
phajsi <phajsi@gmail.com>
2021-10-09 13:33:56 +0200

    Added multiple windows to the app

--- quiz/ui/src/main/java/ui/QuizController.java
+++ quiz/ui/src/main/java/ui/QuizController.java
@@ -1,9 +1,45 @@
 package ui;
 
+import java.io.IOException;
+
 import core.Quiz;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class QuizController {
 
     private Quiz quiz;
 
+    @FXML Button createQuizbtn;
+    @FXML Button startQuizbtn;
+
+public void handleCreateButtonClick() throws IOException{
+
+    try {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CreateQuiz.fxml"));
+        Stage createStage = new Stage();
+        Parent parent = (Parent) fxmlLoader.load();
+        createStage.setScene(new Scene(parent));
+        createStage.show();
+    } catch (IOException e) {
+        throw new IOException("couldn't open window");
+    }
+}
+
+public void handleStartButtonClick() throws IOException{
+
+    try {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StartQuiz.fxml"));
+        Stage createStage = new Stage();
+        Parent parent = (Parent) fxmlLoader.load();
+        createStage.setScene(new Scene(parent));
+        createStage.show();
+    } catch (IOException e) {
+        throw new IOException("couldn't open window");
+    }
+}
 }
